ccb65a5ed4a78577ba3e0191baef1df0796eb37b
Damian Velazquez <osdamv@gmail.com>
2017-12-10 14:53:45 -0700

    login and register accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :person, optional: true 
   accepts_nested_attributes_for :person
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208191834) do
+ActiveRecord::Schema.define(version: 20171210210611) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "person_id"
@@ -60,6 +60,10 @@ ActiveRecord::Schema.define(version: 20171208191834) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["person_id"], name: "index_users_on_person_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
