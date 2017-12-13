fbc7b3cf3656e861b1577af55f69f5d80b57312c
Damian Velazquez <osdamv@gmail.com>
2017-12-12 21:44:12 -0700

    redirect forbidden access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.html { redirect_to  session[:return_to] ||= request.referer, notice: 'No tienes acceso a esa pagina' }
       format.js   { head :forbidden, content_type: 'text/html' }
     end
   end
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   protected  
   def after_sign_in_path_for(resource)
     if current_user.projects.any?
-      # change this to hte active project
+      # change this to hte active projects
        edit_project_path current_user.projects.first
     else
        new_project_path
