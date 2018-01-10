10da053cd18b26646f1cb8639de78e1d65e26a31
salvador <salvador@debian.sal-pc>
2018-01-09 23:09:55 -0700

    fix folio in proyects datatable

--- app/datatables/project_datatable.rb
+++ app/datatables/project_datatable.rb
@@ -9,18 +9,20 @@ class ProjectDatatable < AjaxDatatablesRails::Base
     @view_columns ||= {
       # id: { source: "User.id", cond: :eq },
       # name: { source: "User.name", cond: :like }
-      folio:      { source: "Project.folio",    cond: :like,searchable: true, orderable: true },
+      folio:      { source: "fol",  cond: folio_cond ,searchable: true, orderable: true},
       user:       { source: "User.first_name",  cond: :like,searchable: true},
       category:   { source: "Category.name" ,   cond: :like,searchable: true},
       #show:       { source: "Project.category.name" },
       #edit:       { source: "Project.status"},
     }
+
+
   end
 
   def data
     records.map do |record|
       {
-        folio:  record.folio,
+        folio:  record[:fol],
         user:   record.user.first_name,
         category: record.category.name,
         show: link_to( 'Mostrar', record, :class=>'waves-effect waves-light btn',:style=>'color:white'),
@@ -35,11 +37,24 @@ class ProjectDatatable < AjaxDatatablesRails::Base
 
 
   def document_validation(id)
+    unless Project.where(id: id).first.blank?
+      @valid=Project.where(id: id).first.is_valid?
+      @invalid = Project.where(id: id).first.invalid_revisions.pluck(:field,:observations)
+      validation = @invalid.empty? ? "Pendiente":"Invalido"
+      @valid ? "Valido": validation
+    end
+  end
 
-    @valid=Project.where(id: id).first.is_valid?
-    @invalid = Project.where(id: id).first.invalid_revisions.pluck(:field,:observations)
-    validation = @invalid.empty? ? "Pendiente":"Invalido"
-    @valid ? "Valido": validation
+
+  SEPARATOR = Arel::Nodes.build_quoted(' ') # inside def throw error
+  def folio_cond
+    ->(column) do
+      fol = Arel::Nodes::NamedFunction.new(
+          'concat',
+      [::Arel::Nodes::SqlLiteral.new("categories.key"),SEPARATOR,::Arel::Nodes::SqlLiteral.new("art_forms.name"),SEPARATOR,::Arel::Nodes::SqlLiteral.new("projects.id")]
+      )
+      fol.matches "%#{column}%"           # match change to matches and add the % % flags
+    end
   end
 
   private
@@ -47,7 +62,9 @@ class ProjectDatatable < AjaxDatatablesRails::Base
   def get_raw_records
     # insert query here
     #Project.references(:user).all
-    Project.includes(:user, :category).all.references(:user, :category)
+
+    #Project.includes(:user, :category,:art_form).all.references(:user, :category)
+    Project.select('* , concat(categories.key,"-",art_forms.name,"-",projects.id) as fol').joins(:user, :category, :art_forms)
 
   end
 
