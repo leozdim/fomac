97922478025b07596c3c35a3c6f5a7efca945538
Damian Velazquez <osdamv@gmail.com>
2018-01-20 19:41:25 -0700

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {:host => ENV["EMAIL_DOMAIN"] }
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => 25,
-    :domain  => ENV["EMAIL_DOMAIN"] 
+    :address => ENV["SMTP_ADDRESS"],
+    :port    => 587,
+    :domain  => ENV["EMAIL_DOMAIN"],
+    :user_name => ENV["SMTP_USERNAME"], 
+    :password => ENV["SMTP_PASSWORD"],
+    :authentication => :login,
+    :enable_starttls_auto => true
   }
   config.action_mailer.sendmail_settings = {
     location: '/usr/sbin/sendmail',
